543e097615697651f6a81e88c58afc07c3342c1a
jasonlee529 <819151997@163.com>
2016-01-27 19:32:41 +0800

    配置文件修改，加入springmvc 后类需要改变

--- webflow/src/main/java/cn/infisa/jason/demo/web/SpringController.java
+++ webflow/src/main/java/cn/infisa/jason/demo/web/SpringController.java
@@ -1,15 +1,26 @@
 package cn.infisa.jason.demo.web;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping(value = "/spring/")
+@RequestMapping(value = "/spring")
 public class SpringController {
+	
+	private Log logger = LogFactory.getLog(this.getClass());
 	
 	@RequestMapping
 	public String execute(){
+		logger.info(" execute index ");
+		return "index";
+	}
+	
+	@RequestMapping(value="shopping.do")
+	public String shopping(){
+		logger.info(" spring mvc handle url mapping ");
 		return "index";
 	}
 }
